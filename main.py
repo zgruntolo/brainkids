ee993570a19c374d2187ca2a82921014dd2efcc8
zgruntolo <103127204+zgruntolo@users.noreply.github.com>
2024-05-28 19:39:00 +0200

    update main and added order

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import operations
+import order
 
 if __name__ == '__main__':
-    operations.generatore_divisioni()
+    order.lista_crescente()
+    order.lista_decrescente()
--- operations.py
+++ operations.py
@@ -1,16 +1,25 @@
 import random
+import textwrap
 
+def genera_divisione():
+    dividendo = random.randint(10, 60)
+    divisore = random.randint(2, 10)
+    
+    print(f"\n{dividendo}:{divisore}=", end="\n\n")
+    for line in textwrap.wrap('*' * dividendo, 10):
+        print(line)
+    
 def generatore_divisioni():
-    divisioni = int(input("Quante divisioni vuoi generare? "))
-    for i in range(divisioni):
-        dividendo = random.randint(10, 60)
-        divisore = random.randint(2, 10)
-        print(f"\n{dividendo}:{divisore}=", end=2*"\n")
-        for j in range(dividendo):
-            print("*", end="")
-            if (j + 1) % 10 == 0:
-                print()
-        if dividendo % 10 != 0:
-            print()
+    while True:
+        try:
+            divisioni = int(input("Quante divisioni vuoi generare? "))
+            if divisioni <= 0:
+                raise ValueError("Il numero deve essere positivo.")
+            break
+        except ValueError as e:
+            print(f"Input non valido: {e}. Per favore inserisci un numero intero positivo.")
+
+    for _ in range(divisioni):
+        genera_divisione()
 
     input("Premi invio per uscire...")
